409d15b2e9acc9d580d603eb70d41457055afd22
frank1738 <franklineosoro08@gmail.com>
2022-06-13 20:05:28 +0300

    fetch mission data

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import Header from './Components/Header';
 import Rockets from './Components/Rockets';
 import Missions from './Components/Missions';
 import Profile from './Components/Profile';
+import { Provider } from 'react-redux';
+import { store } from './Redux/Missions/missions';
 
 function App() {
   return (
     <div className="app">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Rockets />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Rockets />} />
+            <Route path="/missions" element={<Missions />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </Router>
+      </Provider>
     </div>
   );
 }
--- src/Components/Rockets.js
+++ src/Components/Rockets.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { useDispatch } from 'react-redux/es/exports';
+import { fetchMissions } from '../Redux/Missions/missions';
 
-const Rockets = () => <div>Rockets</div>;
+const Rockets = () => {
+  const dispatch = useDispatch();
+  window.addEventListener('load', () => dispatch(fetchMissions()));
+  return <div>Rockets</div>;
+};
 
 export default Rockets;
