e7e87626d7029eaa3007aca2433a0064e5cf4d57
yasin <yasin.elsharawi23@gmail.com>
2022-06-15 14:30:30 +0300

    Display Reserved Rockets

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => <div>profil</div>;
+const Profile = () => {
+  const rockets = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+
+  return (
+    <div className="profile-container">
+      <div className="joined-missions">
+        <h2>My Missions</h2>
+      </div>
+      <div className="reserved-rockets">
+        <h2>My Rockets</h2>
+        {reservedRockets.map((rocket) => {
+          return (
+            <p key={rocket.id} className="reserved-rocket">
+              {rocket.rocket_name}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
